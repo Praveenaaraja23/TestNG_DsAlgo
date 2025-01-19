59a37682c03bd9e70b30c32f0ff731b162ad3df2
Priya <priyadarsini.thayuman@gmail.com>
2025-01-19 14:03:56 -0800

    Ds and Graph code push

--- src/test/java/testCases/Datastructures_Test.java
+++ src/test/java/testCases/Datastructures_Test.java
@@ -1,5 +1,48 @@
 package testCases;
 
-public class Datastructures_Test {
+import java.io.IOException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import baseTest.BaseTests;
+import utils.LoggerLoad;
+import pageFactory.LoginPage;
+import pageFactory.DatastructuresPage;
 
+public class Datastructures_Test extends BaseTests {
+	DatastructuresPage datas;
+	BaseTests base;
+
+	@BeforeMethod
+	@Parameters("browser")
+	public void enterdataspage() throws InvalidFormatException, IOException {
+		base = new BaseTests();
+		base.validlogin();
+		datas = new DatastructuresPage();
+		datas.getStarted();
+		datas.click_Timecomplexity_dspage();
+	}
+
+	@Test(dataProvider = "DatastructuresTestData", dataProviderClass = DatastructuresPage.class, priority = 1)
+	public void testdatasPage(String linkText, String Code, String ExpectedOutput) {
+		// Navigate to the link based on linkText
+		datas.navigateToLink(linkText);
+		datas.click_Tryherebtn();
+		datas.Entercode_Tryeditor(Code);
+		datas.runbtn();
+		String ActulaResult = datas.getActualmsg();
+		Assert.assertEquals(ActulaResult, ExpectedOutput, "Validation failed for  code");
+		LoggerLoad.info("Actual result  :" + ActulaResult);
+		LoggerLoad.info("ExpectedOutput  :" + ExpectedOutput);
+	}
+
+	@Test(priority = 2)
+	public void testPracticequestions() {
+		datas.click_Practice_Questions();
+
+		Assert.assertEquals(datas.Homepagetext(), "practice Questions");
+		LoggerLoad.info("NO questions found ");
+	}
 }
